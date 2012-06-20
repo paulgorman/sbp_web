c926fd49983220e2f2694723ec17df52f7d54604
Presence <presence@irev.net>
2012-06-20 02:01:58 -0700

    Using PHP 5.4 now, updated mysqli stuff

--- shared.php
+++ shared.php
@@ -13,23 +13,23 @@
 function Init() {
   global $conn;
 	error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+	date_default_timezone_set('America/Los_Angeles');
 	$host  = "localhost";
 	$db    = "sbpweb";
 	require_once("db.php");
-  $conn = mysql_pconnect($host, $user, $pass) or die(mysql_error());
-  mysql_select_db($db);
+  $conn = mysqli_connect($host, $user, $pass, $db) or die(mysqli_error());
 }
 
 function RecordHit() {
 	global $conn;
 	$query = sprintf("INSERT INTO sitehits (hit_datetime, hit_ip, hit_url, user_agent, referrer) values ('%s','%s','%s','%s','%s');",
-		mysql_real_escape_string(DatePHPtoSQL(time())),
-		mysql_real_escape_string($_SERVER['REMOTE_ADDR']),
-		mysql_real_escape_string($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']),
-		mysql_real_escape_string($_SERVER['HTTP_USER_AGENT']),
-		mysql_real_escape_string($_SERVER['HTTP_REFERRER'])
+		mysqli_real_escape_string($conn, DatePHPtoSQL(time())),
+		mysqli_real_escape_string($conn, $_SERVER['REMOTE_ADDR']),
+		mysqli_real_escape_string($conn, $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']),
+		mysqli_real_escape_string($conn, $_SERVER['HTTP_USER_AGENT']),
+		mysqli_real_escape_string($conn, $_SERVER['HTTP_REFERRER'])
 	);
-	$result = mysql_query($query,$conn);
+	$result = mysqli_query($conn, $query);
 }
 
 function DateSQLtoPHP($mysqldate) {
