8786e91bbb7e9cd48eb138b0a8684e5ef1337f56
Presence <presence@irev.net>
2012-06-20 00:17:35 -0700

    Adding tables to the database

--- sbp_schema.sql
+++ sbp_schema.sql
@@ -1,7 +1,45 @@
-CREATE TABLE `sbpweb`.`artists (
+CREATE TABLE `sbpweb`.`artists` (
 	`aid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(50) NOT NULL,
 	`slug` VARCHAR(255) NOT NULL,
 	`is_active` BOOLEAN NOT NULL DEFAULT 0,
 	`is_highlighted` BOOLEAN NOT NULL DEFAULT 0
 ) ENGINE=InnoDB;
+
+CREATE TABLE `sbpweb`.`artistcategories` (
+	`aid` INT NOT NULL,
+	`cid` INT NOT NULL,
+	INDEX(`aid`),
+	INDEX(`cid`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sbpweb`.`artistmembers` (
+	`aid` INT NOT NULL,
+	`first` VARCHAR(30) NOT NULL,
+	`last` VARCHAR(30) NOT NULL,
+	`phone` CHAR(10),
+	`email` VARCHAR(50),
+	INDEX(`aid`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sbpweb`.`artistlocations` (
+	`aid` INT NOT NULL, 
+	`lid` INT NOT NULL,
+	INDEX(`aid`),
+	INDEX(`lid`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sbpweb`.`locations` (
+	`lid` INT NOT NULL PRIMARY KEY,
+	`city` VARCHAR(40) NOT NULL,
+	`state` CHAR(2) NOT NULL,
+	INDEX(`city`),
+	INDEX(`state`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sbpweb`.`categories` (
+	`cid` INT NOT NULL PRIMARY KEY,
+	`category` VARCHAR(40) NOT NULL UNIQUE,
+	`description` VARCHAR(250)
+) ENGINE=InnoDB;
+
