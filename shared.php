8d37e41026ddbd209aba187d7d3bca82aa510ac1
Presence <presence@irev.net>
2012-08-08 16:13:12 -0700

    Move that password hashing stuff to correct file

--- shared.php
+++ shared.php
@@ -361,3 +361,25 @@ function aasort (&$array, $key) {
 	$array=$ret;
 }
 
+//Takes a password and returns the salted hash
+//$password - the password to hash
+//returns - the hash of the password (192 hex characters)
+function HashPassword($password) {
+    $salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM)); //get 256 random bits in hex
+    $hash = hash("sha512", $salt . $password); //prepend the salt, then hash
+    //store the salt and hash in the same string, so only 1 DB column is needed
+    $final = $salt . $hash;
+    return $final;
+}
+
+//Validates a password
+//$hash - the hash created by HashPassword (stored in your DB)
+//$password - the password to verify
+//returns - true if the password is valid, false otherwise.
+function ValidatePassword($password, $correctHash) {
+    $salt = substr($correctHash, 0, 64); //get the salt from the front of the hash
+    $validHash = substr($correctHash, 64, 128); //the SHA512
+    $testHash = hash("sha256", $salt . $password); //hash the password being tested
+    //if the hashes are exactly the same, the password is valid
+    return $testHash === $validHash;
+}
