711aca3724b4b56beb8a9bc861873361445840f1
Presence <presence@irev.net>
2014-01-27 00:19:15 -0800

    Artist Page

---
+++ templates/artistpage.php
@@ -0,0 +1,9 @@
+<?
+function htmlArtistPageTop($artistinfo) {
+	echo "<div style='max-width:940px;margin: 22px auto 22px auto;'>upper body</div>";
+}
+
+
+function htmlArtistPageBottom($artistinfo) {
+	echo "<div style='height: 10em; margin-top: 1em;'>lower body</div>";
+}
